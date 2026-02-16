c4f41c9a6577060844dbc7956dbd52a2f018acf9
xiao pan <1375126174@qq.com>
2026-02-16 14:23:16 +0800

    首次提交

---
+++ backend/web/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from web.models.user import UserProfile
+from web.models.character import Character
+from web.models.friend import Friend
+
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user',)  #逗号千万不要删！！！！
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
+
+
+@admin.register(Friend)
+class FriendAdmin(admin.ModelAdmin):
+    raw_id_fields = ('me', 'character',)
